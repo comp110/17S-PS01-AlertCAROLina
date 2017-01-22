5bacf4564df8e3b70ffda9b363677a7425c36479
Kris Jordan <krisjordan@gmail.com>
2017-01-21 22:50:41 -0500

    Update to include headers.

--- src/comp110/CarolQuest.java
+++ src/comp110/CarolQuest.java
@@ -1,5 +1,14 @@
 package comp110;
 
+/*
+ * Author: <YOUR NAME>
+ *
+ * ONYEN: <YOUR ONYEN>
+ *
+ * UNC Honor Pledge: I certify that no unauthorized assistance has been 
+ * received or given in the completion of this work. I collaborated with
+ * no one other than official COMP110 UTAs on this code.
+ */
 public class CarolQuest {
 
 	public static void main(String[] args) {
